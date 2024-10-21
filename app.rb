9a441b366da3312c5a1172f3514bcb486a02d6b7
latifvaliyev <102619604+latifvaliyev@users.noreply.github.com>
2024-10-21 18:19:43 +0000

    2

--- app.rb
+++ app.rb
@@ -2,78 +2,44 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
 end
 
 get ("/rock") do
   moves = ["rock","paper","scissors"]
-  comp_move = moves.sample
-  if comp_move == "rock"
-    outcome = "tied"
-  elsif comp_move == "paper"
-    outcome = "lost"
-  else comp_move == "scissors"
-    outcome = "won"
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else @comp_move == "scissors"
+    @outcome = "won"
   end
-
-  "
-  <h2>
-    We played rock!
-  </h2>
-  <h2>
-    They played #{comp_move}!
-  </h2>
-  <h2>
-    We #{outcome}!
-  </h2>
-  "
+erb(:zebra)
 end
+
 get ("/paper") do
   moves = ["rock","paper","scissors"]
-  comp_move = moves.sample
-  if comp_move == "rock"
-    outcome = "won"
-  elsif comp_move == "paper"
-    outcome = "tied"
-  else comp_move == "scissors"
-    outcome = "lost"
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else @comp_move == "scissors"
+    @outcome = "lost"
   end
-
-  "
-  <h2>
-    We played paper!
-  </h2>
-  <h2>
-    They played #{comp_move}!
-  </h2>
-  <h2>
-    We #{outcome}!
-  </h2>
-  "
+erb(:giraffe)
 end
+
 get ("/scissors") do
   moves = ["rock","paper","scissors"]
-  comp_move = moves.sample
-  if comp_move == "rock"
-    outcome = "lost"
-  elsif comp_move == "paper"
-    outcome = "won"
-  else comp_move == "scissors"
-    outcome = "tied"
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else @comp_move == "scissors"
+    @outcome = "tied"
   end
-
-  "
-  <h2>
-    We played scissors!
-  </h2>
-  <h2>
-    They played #{comp_move}!
-  </h2>
-  <h2>
-    We #{outcome}!
-  </h2>
-  "
+erb(:lion)
 end
