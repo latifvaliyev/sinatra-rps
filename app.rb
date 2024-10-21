4ca73df9c637d49cf22aacf23c0c1d470de9a8fb
latifvaliyev <102619604+latifvaliyev@users.noreply.github.com>
2024-10-21 17:54:06 +0000

    1

--- app.rb
+++ app.rb
@@ -7,3 +7,73 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get ("/rock") do
+  moves = ["rock","paper","scissors"]
+  comp_move = moves.sample
+  if comp_move == "rock"
+    outcome = "tied"
+  elsif comp_move == "paper"
+    outcome = "lost"
+  else comp_move == "scissors"
+    outcome = "won"
+  end
+
+  "
+  <h2>
+    We played rock!
+  </h2>
+  <h2>
+    They played #{comp_move}!
+  </h2>
+  <h2>
+    We #{outcome}!
+  </h2>
+  "
+end
+get ("/paper") do
+  moves = ["rock","paper","scissors"]
+  comp_move = moves.sample
+  if comp_move == "rock"
+    outcome = "won"
+  elsif comp_move == "paper"
+    outcome = "tied"
+  else comp_move == "scissors"
+    outcome = "lost"
+  end
+
+  "
+  <h2>
+    We played paper!
+  </h2>
+  <h2>
+    They played #{comp_move}!
+  </h2>
+  <h2>
+    We #{outcome}!
+  </h2>
+  "
+end
+get ("/scissors") do
+  moves = ["rock","paper","scissors"]
+  comp_move = moves.sample
+  if comp_move == "rock"
+    outcome = "lost"
+  elsif comp_move == "paper"
+    outcome = "won"
+  else comp_move == "scissors"
+    outcome = "tied"
+  end
+
+  "
+  <h2>
+    We played scissors!
+  </h2>
+  <h2>
+    They played #{comp_move}!
+  </h2>
+  <h2>
+    We #{outcome}!
+  </h2>
+  "
+end
